3da07e87ffa9eed68bddc79dfa79498d1e59892b
johnsethsalazar <salazarjohnseth@gmail.com>
2024-04-08 17:25:28 +0800

    Added Banner

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>Homepage</div>
+    <>
+    <Image src="/banner.png" alt="banner" width={2000} height={1000} className="w-screen"/>
+    </>
   );
 }
