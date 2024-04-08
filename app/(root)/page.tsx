2c0bb2d8f9b4bcc8c289b601f4991bc799ed7869
johnsethsalazar <salazarjohnseth@gmail.com>
2024-04-08 21:55:07 +0800

    Added the collection component with mapping all created collections in the admin along with a few design applications

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Collections from '@/components/Collections'
+
 
 export default function Home() {
   return (
     <>
     <Image src="/banner.png" alt="banner" width={2000} height={1000} className="w-screen"/>
+    <Collections />
     </>
   );
 }
---
+++ lib/actions.ts
@@ -0,0 +1,6 @@
+import Collections from "@/components/Collections";
+
+export const getCollections = async () => {
+  const collections = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/collections`);
+  return await collections.json();
+}
