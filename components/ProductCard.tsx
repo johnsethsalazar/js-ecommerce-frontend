433060b5e67ef81a4002912dafa79230dceb07f4
johnsethsalazar <salazarjohnseth@gmail.com>
2024-04-09 11:20:43 +0800

    Completed Wishlist function

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,9 +1,62 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
 import { Heart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
+  const router = useRouter();
+  const { user } = useUser();
+
+  const [loading, setLoading] = useState(false);
+  const [signedInUser, setSignedInUser] = useState<UserType | null>(null);
+  const [isLiked, setIsLiked] = useState(false);
+  const getUser = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch("/api/user");
+      const data = await res.json();
+      setSignedInUser(data);
+      setIsLiked(data.wishlist.includes(product._id));
+      setLoading(false);
+    } catch (err) {
+      console.log("[users_GET]", err);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      getUser();
+    }
+  }, [user]);
+
+  const handleLike = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    try {
+      //If user clicked wishlist button and hasn't signed in yet. The user will be redirected to the sign-in page
+      if (!user) {
+        router.push("/sign-in");
+        return;
+      } else {
+        setLoading(true);
+        const res = await fetch("/api/user/wishlist", {
+          method: "POST",
+          body: JSON.stringify({ productId: product._id }),
+        });
+        const updatedUser = await res.json();
+        setSignedInUser(updatedUser.wishlist.includes(product._id));
+      }
+    } catch (err) {
+      console.log("[wishlist_POST]", err);
+    }
+  };
+
   return (
     <Link
       href={`/products/${product._id}`}
@@ -22,8 +75,8 @@ const ProductCard = ({ product }: { product: ProductType }) => {
       </div>
       <div className="flex justify-between items-center">
         <p className="text-body-bold">${product.price}</p>
-        <button>
-          <Heart />
+        <button onClick={handleLike}>
+          <Heart fill={`${isLiked ? "red" : "none"}`} />
         </button>
       </div>
     </Link>
