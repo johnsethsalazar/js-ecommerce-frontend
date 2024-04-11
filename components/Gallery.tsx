d3cc4c67559676b528c33506669a0b1c38e0126d
johnsethsalazar <salazarjohnseth@gmail.com>
2024-04-11 16:44:32 +0800

    added a feature that renders all the product's images under the main image in the /product page

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,12 +1,30 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const Gallery = ({productMedia}: {productMedia: string []}) => {
+const Gallery = ({ productMedia }: { productMedia: string[] }) => {
   return (
-    <div className='flex flex-col gap-3 max-w-[500px]'>
-      <Image src={productMedia[0]} alt="product" width={500} height={500} className='w-96 h-96 rounded-lg shadow-lg object-cover'/>
+    <div className="flex flex-col gap-3 max-w-[500px]">
+      <Image
+        src={productMedia[0]}
+        alt="product"
+        width={500}
+        height={500}
+        className="w-96 h-96 rounded-lg shadow-lg object-cover"
+      />
+      <div className="flex gap-2 overflow-auto tailwind-scrollbar-hide">
+        {productMedia.map((image, index) => (
+          <Image
+            key={index}
+            src={image}
+            alt="product"
+            width={200}
+            height={200}
+            className="w-20 h-20 rounded-lg shadow-lg object-cover cursor-pointer"
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
