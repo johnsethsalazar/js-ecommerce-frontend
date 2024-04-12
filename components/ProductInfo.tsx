16bff1fdba3edc88b62cf2bf5d96c58eea1b2719
johnsethsalazar <salazarjohnseth@gmail.com>
2024-04-12 16:15:10 +0800

    Added the color picker to the ProductInfo.tsx component

--- components/ProductInfo.tsx
+++ components/ProductInfo.tsx
@@ -35,6 +35,22 @@ const ProductInfo = ({ productInfo }: { productInfo: ProductType }) => {
           </div>
         </div>
       )}
+
+      {productInfo.sizes.length > 0 && (
+        <div className="flex flex-col gap-2">
+          <p className="text-base-medium text-grey-2">Sizes</p>
+          <div className="flex gap-2">
+            {productInfo.sizes.map((sizes, index) => (
+              <p
+                key={index}
+                className="border border-black px-2 py-1 rounded-lg cursor-pointer"
+              >
+                {sizes}
+              </p>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
