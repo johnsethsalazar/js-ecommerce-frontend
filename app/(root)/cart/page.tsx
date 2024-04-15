3ce5a22b331ffea3c1f61624350e86db3f8aa10c
johnsethsalazar <salazarjohnseth@gmail.com>
2024-04-15 10:57:46 +0800

    Added item mapping, quantity selector, and delete item functionalities in the /cart page

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
+"use client";
+
+import useCart from "@/lib/hooks/useCart";
+import { MinusCircle, PlusCircle, Trash } from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
 const Cart = () => {
+  const cart = useCart();
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="flex gap-20 py-16 px-10">
+      <div className="w-2/3">
+        <p className="text-heading3-bold">Shopping Cart</p>
+        <hr className="my-6"/>
 
-export default Cart
+        {cart.cartItems.length === 0 ? (
+          <p className="text-body-bold">No item in cart</p>
+        ) : (
+          <div>
+            {cart.cartItems.map((cartItem) => (
+              // eslint-disable-next-line react/jsx-key
+              <div className="w-full flex hover:bg-grey-1 px-6 py-5 items-center justify-between">
+                <div className="flex items-center">
+                  <Image
+                    src={cartItem.item.media[0]}
+                    width={100}
+                    height={100}
+                    className="rounded-lg w-32 h-32 object-cover"
+                    alt="product"
+                  />
+                  <div className="flex flex-col gap-3 ml-4">
+                    <p className="text-body-bold">{cartItem.item.title}</p>
+                    {cartItem.color && (
+                      <p className="text-small-medium">{cartItem.color}</p>
+                    )}
+                    {cartItem.size && (
+                      <p className="text-small-medium">{cartItem.size}</p>
+                    )}
+                  </div>
+                </div>
+
+                <div className="flex gap-4 items-center">
+                  <MinusCircle
+                    className="hover:text-red-1 cursor-pointer"
+                    onClick={() => cart.decreaseQuantity(cartItem.item._id)}
+                  />
+                  <p className="text-body-bold">{cartItem.quantity}</p>
+                  <PlusCircle
+                    className="hover:text-green-600 cursor-pointer"
+                    onClick={() => cart.increaseQuantity(cartItem.item._id)}
+                  />
+                </div>
+
+                <Trash
+                  className="hover:text-red-1 cursor-pointer"
+                  onClick={() => cart.removeItem(cartItem.item._id)}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
