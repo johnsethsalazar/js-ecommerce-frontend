6be6552b2ff13da964aab42030806aba8cb7d85b
johnsethsalazar <salazarjohnseth@gmail.com>
2024-05-06 11:02:08 +0800

    Added redirect function to collection page when the collection page image is clicked

---
+++ app/(root)/collections/[collectionId]/page.tsx
@@ -0,0 +1,33 @@
+import ProductCard from "@/components/ProductCard";
+import { getCollectionDetails } from "@/lib/actions/actions";
+import Image from "next/image";
+import React from "react";
+
+const CollectionDetails = async ({
+  params,
+}: {
+  params: { collectionId: string };
+}) => {
+  const collectionDetails = await getCollectionDetails(params.collectionId);
+  console.log(collectionDetails);
+  return (
+    <div className="px-10 py-5 text-grey-2 flex flex-col items-center gap-8">
+      <Image
+        src={collectionDetails.image}
+        alt="collection"
+        width={1500}
+        height={1000}
+        className="w-full h-[400px] object-cover rounded-xl"
+      />
+      <p className="text-heading3-bold">{collectionDetails.title}</p>
+      <p className="text-body-normal text-center max-w-[900px]">{collectionDetails.description}</p>
+      <div>
+        {collectionDetails.products.map((product: ProductType) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionDetails;
