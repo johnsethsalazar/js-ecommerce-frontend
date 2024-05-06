0c55a6e20ae5f0868acb0034a16442a74fa883a9
johnsethsalazar <salazarjohnseth@gmail.com>
2024-05-06 15:19:53 +0800

    Added personal feature. Display a No products found text in the /collections page when no products is inputted for the collection

--- app/(root)/collections/[collectionId]/page.tsx
+++ app/(root)/collections/[collectionId]/page.tsx
@@ -19,13 +19,23 @@ const CollectionDetails = async ({
         height={1000}
         className="w-full h-[400px] object-cover rounded-xl"
       />
-      <p className="text-heading3-bold text-grey-2">{collectionDetails.title}</p>
-      <p className="text-body-normal text-center max-w-[900px]">{collectionDetails.description}</p>
-      <div className="flex flex-wrap gap-16 mx-auto">
-        {collectionDetails.products.map((product: ProductType) => (
-          <ProductCard key={product._id} product={product} />
-        ))}
-      </div>
+      <p className="text-heading3-bold text-grey-2">
+        {collectionDetails.title}
+      </p>
+      <p className="text-body-normal text-center max-w-[900px]">
+        {collectionDetails.description}
+      </p>
+      {collectionDetails.products.length > 0 ? (
+        <div className="flex flex-wrap gap-16 mx-auto">
+          {collectionDetails.products.map((product: ProductType) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-heading3-bold text-grey-2">
+          No products found for {collectionDetails.title} collection
+        </p>
+      )}
     </div>
   );
 };
