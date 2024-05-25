9dd0725ece268a69bc5cfc659fc1311044f3bffb
johnsethsalazar <salazarjohnseth@gmail.com>
2024-05-25 16:36:56 +0800

    Added the ATC button and it's ATC function to the SATC component

--- components/Satc.tsx
+++ components/Satc.tsx
@@ -23,24 +23,30 @@ const Satc = ({ productMedia, productInfo }: ProductSatcProps) => {
   const [dropdownMenu, setdropdownMenu] = useState(false);
   const [query, setQuery] = useState("");
 
+  const [selectedColor, setSelectedColor] = useState<string>(
+    productInfo.colors[0]
+  );
+  const [selectedSize, setSelectedSize] = useState<string>(
+    productInfo.sizes[0]
+  );
+  const [quantity, setQuantity] = useState<number>(1);
+
   return (
     <div className="sticky bottom-0 z-10 py-2 px-10 flex gap-2 justify-between items-center bg-white max-sm:px-2">
       <Link href="/">
-      <Image
-        src={mainImage}
-        alt="product"
-        width={500}
-        height={500}
-        className="w-24 h-24 rounded-lg shadow-lg object-cover"
-      />
+        <Image
+          src={mainImage}
+          alt="product"
+          width={500}
+          height={500}
+          className="w-24 h-24 rounded-lg shadow-lg object-cover"
+        />
       </Link>
       <div className="flex gap-4 text-base-bold max-lg:hidden w-3/12	">
         <p className="text-heading3-bold text-left ">{productInfo.title}</p>
       </div>
 
       <p className="text-heading3">${productInfo.price}</p>
-
-      
 
       <div className="flex gap-3 border border-grey-2 px-3 py-1 items-center rounded-lg">
         <input
@@ -70,41 +76,19 @@ const Satc = ({ productMedia, productInfo }: ProductSatcProps) => {
           className="cursor-pointer lg:hidden"
           onClick={() => setdropdownMenu(!dropdownMenu)}
         />
-
-        {dropdownMenu && (
-          <div className="absolute top-12 right-5 flex flex-col gap-4 p-3 rounded-lg border bg-white text-base-bold lg:hidden">
-            <Link href="/" className="hover:text-red-1">
-              Home
-            </Link>
-            <Link
-              href={user ? "/wishlist" : "/sign-in"}
-              className="hover:text-red-1"
+        <button
+              className="outline text-base-bold py-3 rounded-lg hover:bg-black hover:text-white"
+              onClick={() =>
+                cart.addItem({
+                  item: productInfo,
+                  quantity,
+                  color: selectedColor,
+                  size: selectedSize,
+                })
+              }
             >
-              Wishlist
-            </Link>
-            <Link
-              href={user ? "/orders" : "/sign-in"}
-              className="hover:text-red-1"
-            >
-              Orders
-            </Link>
-            <Link
-              href="/cart"
-              className="flex items-center gap-3 border rounded-lg px-2 py-1 hover:bg-black hover:text-white"
-            >
-              <ShoppingCart />
-              <p className="text-base-bold">Cart ({cart.cartItems.length})</p>
-            </Link>
-          </div>
-        )}
-
-        {user ? (
-          <UserButton afterSignOutUrl="/sign-in" />
-        ) : (
-          <Link href="/sign-in">
-            <CircleUserRound />
-          </Link>
-        )}
+              Add to Cart
+            </button>
       </div>
     </div>
   );
